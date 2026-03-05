c1fd31ca6cb5acaaf9d5da4bc677d9165a8121bd
Sukrit Jain <sukritj07@gmail.com>
2026-03-05 19:31:39 +0530

    Add selenium E2E test

---
+++ tests/selenium/test_ui.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def test_homepage_loads():
+
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=options)
+
+    driver.get("http://localhost:8080")
+
+    assert "Technical Debt Tracker" in driver.page_source
+
+    driver.quit()
