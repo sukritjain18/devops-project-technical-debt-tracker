9ea8a5867808c6e53e484acca7c99982ee9968d9
Sukrit Jain <sukritj07@gmail.com>
2026-03-05 20:02:51 +0530

    Introduce test failure for CI demonstration

--- tests/unit/test_basic.py
+++ tests/unit/test_basic.py
@@ -4,5 +4,5 @@ def test_home_route():
     client = app.test_client()
     response = client.get("/")
 
-    assert response.status_code == 200
+    assert response.status_code == 500
     assert b"Technical Debt Tracker" in response.data
