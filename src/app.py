179d811d3fa614a4bee62caccb3fb4f6639c7d89
Sukrit Jain <sukritj07@gmail.com>
2026-04-05 20:10:07 +0530

    update email config to use SSL port 465 for Render

--- src/app.py
+++ src/app.py
@@ -22,8 +22,9 @@ API_KEY = os.getenv("API_KEY", "default-api-key")
 
 # Email configuration
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.getenv("MAIL_USERNAME")
 app.config['MAIL_PASSWORD'] = os.getenv("MAIL_PASSWORD")
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv("MAIL_USERNAME")
